764ff9ab5ae6ca33a1702c8c658551244785d526
aditbro061-lab <aditbro061@gmail.com>
2025-10-28 11:25:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// 1. Pilih tombol berdasarkan ID-nya const tombolSapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click tombolSapa.AddEventListener('click',function(){
+// 3. Jalankan kde ini ketika tombol di=klik alert('Halo! Terima kasih sudah berkunjung!');});
